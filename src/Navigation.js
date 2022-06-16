8fb723e158f87ba223bfbf08c21647434f6eccc5
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-06-16 17:39:38 +0100

    add back button and nav style

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import './Navigation.css'
 
 const Navigation = () => {
+    let goTo = useNavigate();
+
     return(
         <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/bands">Bands</NavLink>
-            <NavLink to="/lyrics">Song Lyrics</NavLink>
+            <NavLink to="/" className="link">Home</NavLink>
+            <NavLink to="/bands" className="link">Bands</NavLink>
+            <NavLink to="/lyrics" className="link">Song Lyrics</NavLink>
+            <a onClick={() => goTo(-1)} className="link back">Go back</a>
         </nav>
     )
 }
